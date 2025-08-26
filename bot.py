db5519ca5addcd1ed9050b3942b0617b445b33cf
ContentBakery1 <keke.zuidinga@icloud.com>
2025-08-26 15:54:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,11 @@ import os
 # Haal token uit environment variable
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-bot = commands.Bot(command_prefix="!")
+# Zet de intents die je nodig hebt
+intents = discord.Intents.default()
+intents.message_content = True  # Nodig om commands te gebruiken
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
